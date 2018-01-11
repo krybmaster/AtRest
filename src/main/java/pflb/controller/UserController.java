faeba12d8146958aa07bb180fb28295e6698da39
Yaroslav <karatel-dead2@mail.ru>
2018-01-11 17:29:02 +0400

    Обновил Controller Работающий DELETE запрос Прототип POST запроса

--- src/main/java/pflb/controller/UserController.java
+++ src/main/java/pflb/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import pflb.entity.UserForLogin;
 
 import java.sql.*;
+import java.util.List;
 
 import static pflb.db.Connection.getConnection;
 
@@ -16,8 +17,33 @@ public class UserController {
     private String sql;
 
     @RequestMapping(value = "/auth", method = RequestMethod.POST)
-    public int postAuthUser(@RequestBody UserForLogin user) {
-        //sql запрос на аутентификацию
+    public int postAuthUser(@RequestBody String login, String password) {
+
+        sql = "{CALL dbo.LogIn(?,?)}";
+        Connection con = null;
+        con = getConnection();
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sessionID = "";
+
+        try {
+            pstmt = con.prepareCall(sql);
+            pstmt.setString(1, login);
+            pstmt.setString(2, password);
+            rs = pstmt.executeQuery();
+            rs.next();
+
+            sessionID = rs.getString("SESSION_ID");
+            if (sessionID == "") System.out.println(200);
+            else System.out.println(404);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {rs.close();} catch (SQLException ignored) {}
+            try {pstmt.close();} catch (SQLException ignored) {}
+            try {con.close();} catch (SQLException ignored) {}
+        }
         return ResultCode;
     }
 
@@ -27,21 +53,24 @@ public class UserController {
         return ResultCode;
     }
 
-    @RequestMapping(value = "/auth/session/{id}", method = RequestMethod.DELETE)
-    public int deleteSession(@PathVariable String id){
-        sql = "EXEC dbo.LogOut \'?\'";
+    @RequestMapping(value = "/auth/session/{login}", method = RequestMethod.DELETE)
+    public int deleteSession(@PathVariable String login){
+
+        sql = "{CALL dbo.LogOut(?)}";
         Connection con = null;
         con = getConnection();
         PreparedStatement pstmt = null;
+
         try {
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, id);
+            pstmt = con.prepareCall(sql);
+            pstmt.setString(1, login);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+            ResultCode = 1;
         } finally {
-            try {con.close();} catch (SQLException ignored) {}
             try {pstmt.close();} catch (SQLException ignored) {}
+            try {con.close();} catch (SQLException ignored) {}
         }
 
         return ResultCode;
