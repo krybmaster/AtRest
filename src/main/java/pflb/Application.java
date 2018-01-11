b3a6221efbb82cc0a4336d7012f19af44bdbcb15
Yaroslav <karatel-dead2@mail.ru>
2018-01-11 11:31:26 +0400

    Обновил application. Убрал подключение к БД, т.к. его реализация планируется в контроллере.

--- src/main/java/pflb/Application.java
+++ src/main/java/pflb/Application.java
@@ -13,37 +13,6 @@ import static pflb.db.Connection.getConnection;
 public class Application {
 
     public static void main(String[] args) {
-
-        Connection con = getConnection();
-        Statement stmt = null;
-        ResultSet rs;
-        String query = "SELECT LOGIN FROM users";
-
-        try {
-            stmt = con.createStatement();
-            rs = stmt.executeQuery(query);
-
-            while (rs.next()){
-                String login = rs.getString("LOGIN");
-
-                System.out.println("Login: " + login);
-            }
-            rs.close();
-        } catch (SQLException se) {
-            se.printStackTrace();
-        } finally {
-            try{
-                if(stmt!=null)
-                    con.close();
-            }catch(SQLException se){}
-            try{
-                if(con!=null)
-                    con.close();
-            }catch(SQLException se){
-                se.printStackTrace();
-            }
-        }
-
-        //SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
