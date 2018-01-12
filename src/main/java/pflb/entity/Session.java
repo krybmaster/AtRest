f6aa96add2171f6ad9d658a90fd79840c43ffe15
Yaroslav <karatel-dead2@mail.ru>
2018-01-12 13:57:41 +0400

    Создал entity Session. В нем инициализируется ID сессии.

---
+++ src/main/java/pflb/entity/Session.java
@@ -0,0 +1,18 @@
+package pflb.entity;
+
+public class Session {
+
+    private String sessionID;
+
+    public Session(String sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    public String getSessionID() {
+        return sessionID;
+    }
+
+    public void setSessionID(String sessionID) {
+        this.sessionID = sessionID;
+    }
+}
