96fe460df64bbfea23824824b205c9aaaca65132
Yaroslav <karatel-dead2@mail.ru>
2018-01-12 13:59:50 +0400

    Обновил Controller Реализовал Post запрос, возвращающий sessionID в JSON. Заготовка для запроса GET.

--- src/main/java/pflb/controller/UserController.java
+++ src/main/java/pflb/controller/UserController.java
@@ -1,41 +1,44 @@
 package pflb.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.google.gson.*;
 import org.springframework.web.bind.annotation.*;
-import pflb.entity.User;
+import pflb.entity.Session;
 import pflb.entity.UserForLogin;
+import pflb.entity.UserInfo;
 
 import java.sql.*;
-import java.util.List;
 
 import static pflb.db.Connection.getConnection;
 
 @RestController
 public class UserController {
 
+    final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     private int ResultCode;
     private String sql;
 
     @RequestMapping(value = "/auth", method = RequestMethod.POST)
-    public int postAuthUser(@RequestBody UserForLogin user) {
+    public String postAuthUser(@RequestBody String JsonReq) throws JsonParseException {
 
         sql = "{CALL dbo.LogIn(?,?)}";
         Connection con = null;
         con = getConnection();
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-        String sessionID = "";
+        String sessionIDJson = "";
 
         try {
+
+            UserForLogin user = GSON.fromJson(JsonReq, UserForLogin.class);
+
             pstmt = con.prepareCall(sql);
-            pstmt.setString(1, user.getLOGIN());
-            pstmt.setString(2, user.getPASSMD5());
+            pstmt.setString(1, user.getLogin());
+            pstmt.setString(2, user.getPassMD5());
             rs = pstmt.executeQuery();
             rs.next();
 
-            sessionID = rs.getString("SESSION_ID");
-            if (sessionID == "") ResultCode = 1;
+            Session sessionReq = new Session(rs.getString("SESSION_ID"));
+            sessionIDJson = GSON.toJson(sessionReq);
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -44,13 +47,27 @@ public class UserController {
             try {pstmt.close();} catch (SQLException ignored) {}
             try {con.close();} catch (SQLException ignored) {}
         }
-        return ResultCode;
+        return sessionIDJson;
     }
 
     @RequestMapping(value = "/user/session/{id}", method = RequestMethod.GET)
-    public int getUserInfo(@PathVariable String id){
-        //sql запрос на получение информации о пользователе
-        return ResultCode;
+    public String getUserInfo(@PathVariable String sessionID) throws JsonParseException{
+
+        sql = "{CALL dbo.GetUser(?)}";
+        Connection con = null;
+        con = getConnection();
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String infoReq = "";
+
+        try {
+            pstmt = con.prepareCall(sql);
+            pstmt.setString(1,sessionID);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return infoReq;
     }
 
     @RequestMapping(value = "/auth/session/{login}", method = RequestMethod.DELETE)
@@ -60,6 +77,8 @@ public class UserController {
         Connection con = null;
         con = getConnection();
         PreparedStatement pstmt = null;
+
+        System.out.println(login);
 
         try {
             pstmt = con.prepareCall(sql);
