67fc13a77611aa23f4c8a6a8c113ce3c680f7bb5
Yaroslav <karatel-dead2@mail.ru>
2018-01-15 18:13:00 +0400

    Rename

--- src/main/java/pflb/Json/Serializers/EmptyReqSerializer.java
+++ src/main/java/pflb/Json/Serializers/EmptyReqSerializer.java
@@ -9,7 +9,7 @@ import pflb.entity.User;
 
 import java.lang.reflect.Type;
 
-public class AuthSerializerWithError implements JsonSerializer<User> {
+public class EmptyReqSerializer implements JsonSerializer<User>{
 
     @Override
     public JsonElement serialize(User src, Type typeOfStc, JsonSerializationContext context) {
