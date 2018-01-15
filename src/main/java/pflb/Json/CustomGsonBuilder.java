5b4d9bc836e8b0c7f97eb850ecde62061389ef00
Yaroslav <karatel-dead2@mail.ru>
2018-01-15 14:02:39 +0400

    Объеденил все GsonBuilders в один класс, для дальнейшего наследования.

---
+++ src/main/java/pflb/Json/CustomGsonBuilder.java
@@ -0,0 +1,32 @@
+package pflb.Json;
+
+import com.google.gson.*;
+import pflb.Json.Deserializer.AuthErrorDeserializer;
+import pflb.Json.Serializers.AuthSerializer;
+import pflb.Json.Serializers.AuthSerializerWithError;
+import pflb.entity.User;
+
+public class CustomGsonBuilder {
+
+
+    protected Gson AuthErrorFromJson() {
+        return new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(User.class, new AuthErrorDeserializer())
+                .create();
+    }
+
+    protected Gson AuthReqJsonWithError() {
+        return new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(User.class, new AuthSerializerWithError())
+                .create();
+    }
+
+    protected Gson AuthReqJson(){
+        return new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(User.class, new AuthSerializer())
+                .create();
+    }
+}
