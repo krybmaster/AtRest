1081ba2cb2d1bbbeb69c661b3ed3b0bbcd5ee5d0
Yaroslav <karatel-dead2@mail.ru>
2018-01-29 12:24:22 +0400

    Add image url from serializer

--- src/main/java/pflb/json/Serializers/InfoSerializer.java
+++ src/main/java/pflb/json/Serializers/InfoSerializer.java
@@ -18,6 +18,7 @@ public class InfoSerializer implements JsonSerializer<User>{
         result.addProperty("last_name", src.getLastName());
         result.addProperty("middle_name", src.getMiddleName());
         result.addProperty("role", src.getRole());
+        result.addProperty("img_url", src.getImgURL());
         result.addProperty("message", src.getReqMessage());
         result.addProperty("code", src.getReturnCode());
 
